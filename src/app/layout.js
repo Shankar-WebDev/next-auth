8eb205b958b55575d06979c6525e3fb028121005
Sankaranarayanan <sankar241101@gmail.com>
2025-01-08 13:48:39 +0530

    Add Authentication using Clerk

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,31 +1,40 @@
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
-import Header from "@/components/Header";
+import { Geist, Geist_Mono } from 'next/font/google';
+import './globals.css';
+import Header from '@/components/Header';
+import { ClerkProvider, ClerkLoaded, ClerkLoading } from '@clerk/nextjs';
+import Loader from '@/components/Loader';
 
 const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
+  variable: '--font-geist-sans',
+  subsets: ['latin'],
 });
 
 const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+  variable: '--font-geist-mono',
+  subsets: ['latin'],
 });
 
 export const metadata = {
-  title: "Next Auth",
-  description: "Next auth with clerk and MongoDB",
+  title: 'Next Auth',
+  description: 'Next auth with clerk and MongoDB',
 };
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        <Header/>
-        {children}
-      </body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body
+          className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+          <ClerkLoading>
+          <Loader/>
+          </ClerkLoading>
+          <ClerkLoaded>
+            <Header />
+         
+            {children}
+          </ClerkLoaded>
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Home() {
   return (
-    <div className='text-red-500'>Home</div>
+    <div className='text-red-500 flex justify-center items-center'>Home</div>
   )
 }
 
