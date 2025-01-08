7de5371cfa7b8f9f06ad14ce7346cfb8ae63bce5
Sankaranarayanan <sankar241101@gmail.com>
2025-01-08 18:06:53 +0530

    final Updatation

--- src/lib/actions/user.js
+++ src/lib/actions/user.js
@@ -1,42 +1,44 @@
-import User from "../models/user.model";
-import {connect} from '../mongodb/mongoose'
+import User from '../models/user.model';
 
+import { connect } from '../mongodb/mongoose';
 
-export const createOrUpdatedUser = async(
-    id,
-    first_name,
-    last_name,
-    image_url,
-    email_addresses,
-    username
-)=>{
-    try{
+export const createOrUpdateUser = async (
+  id,
+  first_name,
+  last_name,
+  image_url,
+  email_addresses,
+  username
+) => {
+  try {
     await connect();
+
     const user = await User.findOneAndUpdate(
-        {clerkId:id},{
-            $set:{
-                firstName:first_name,
-                lastName: last_name,
-                avatar:image_url,
-                email:email_addresses[0].email,
-                username:username
-            }
+      { clerkId: id },
+      {
+        $set: {
+          firstName: first_name,
+          lastName: last_name,
+          avatar: image_url,
+          email: email_addresses[0].email,
+          username: username,
         },
-        {new:true, upsert:true}
-    )
+      },
+      { new: true, upsert: true }
+    );
+
     return user;
-}
-catch(error){
-    console.log('Error creating or Updating User:',error)
-}
+  } catch (error) {
+    console.log('Error creating or updating user:', error);
+  }
 };
 
+export const deleteUser = async (id) => {
+  try {
+    await connect();
 
-export const deleteUser = async(id) =>{
-    try{
-        await connect();
-        await User.findOneAndDelete({clerkId:id});
-    }catch(error){
-        console.log('Error deleting user:',error)
-    }
-}
+    await User.findOneAndDelete({ clerkId: id });
+  } catch (error) {
+    console.log('Error deleting user:', error);
+  }
+};
